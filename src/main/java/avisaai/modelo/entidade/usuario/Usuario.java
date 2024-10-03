fbd06236f385819ca757bcdc6cf0156257a9f057
Daiane Darosci <daiane.darosci@gmail.com>
2024-10-03 06:50:30 -0300

    Criada anotações JPA da classe Usuário

--- src/main/java/avisaai/modelo/entidade/usuario/Usuario.java
+++ src/main/java/avisaai/modelo/entidade/usuario/Usuario.java
@@ -5,44 +5,81 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import avisaai.modelo.entidade.comentario.Comentario;
+import avisaai.modelo.entidade.comunidade.Comunidade;
+import avisaai.modelo.entidade.foto.Foto;
 import avisaai.modelo.entidade.incidente.Incidente;
+import avisaai.modelo.entidade.usuario.contato.Contato;
 import avisaai.modelo.entidade.usuario.papel.Papel;
 
+@Entity
+@Table(name = "usuario")
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = -2518745741977867337L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
 	private Long id;
 
+	@Column(name = "nome_usuario", length = 30, nullable = false)
 	private String nome;
 
+	@Column(name = "sobrenome_usuario", length = 30, nullable = false)
 	private String sobrenome;
 
+	@Column(name = "senha", length = 16, nullable = false)
 	private String senha;
 
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_contato", referencedColumnName = "id_contato")
 	private Contato contato;
 
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_foto", referencedColumnName = "id_foto")
 	private Foto fotoPerfil;
 
+	@OnetoOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_papel", referencedColumnName = "id_papel")
 	private Papel papel;
 
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "usuarios_membros_comunidades", joinColumns = @JoinColumn(name = "id_usuario"), inverseJoinColumns = @JoinColumn(name = "id_comunidade"))
 	private List<Comunidade> comunidadesAcompanhadas;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Comentario> comentariosFeitos;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Incidente> incidentesCadastrados;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Incidente> incidentesAcompanhados;
 
 	public Usuario() {
 	}
 
-	public Usuario(String nome, String sobrenome, String senha, Contato contato, Foto foto, Papel papel) {
+	public Usuario(String nome, String sobrenome, String senha, Contato contato, Foto fotoPerfil, Papel papel) {
 		setNome(nome);
 		setSobrenome(sobrenome);
 		setSenha(senha);
 		setContato(contato);
-		setFoto(foto);
+		setFotoPerfil(fotoPerfil);
 		setPapel(papel);
 		comunidadesAcompanhadas = new ArrayList<Comunidade>();
 		comentariosFeitos = new ArrayList<Comentario>();
@@ -106,14 +143,6 @@ public class Usuario implements Serializable {
 		this.papel = papel;
 	}
 
-	public void adicionarPapel(Papel papel) {
-		this.papeis.add(papel);
-	}
-
-	public void removerPapel(Papel papel) {
-		this.papeis.remove(papel);
-	}
-
 	public List<Comunidade> getComunidadesAcompanhadas() {
 		return comunidadesAcompanhadas;
 	}
@@ -175,6 +204,6 @@ public class Usuario implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Usuario outro = (Usuario) obj;
-		return Objects.equals(id, outro, id);
+		return id == outro.id;
 	}
 }
