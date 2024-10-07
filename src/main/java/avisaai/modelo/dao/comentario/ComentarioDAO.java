6232b14f241cece03e577d98cf2800fbbaf1fdbe
Willian Silva <willian.stela@hotmail.com>
2024-10-07 12:24:57 -0300

    Criado DAO da classe Comentário

---
+++ src/main/java/avisaai/modelo/dao/comentario/ComentarioDAO.java
@@ -0,0 +1,27 @@
+package avisaai.modelo.dao.comentario;
+
+import java.util.List;
+
+import avisaai.modelo.entidade.comentario.Comentario;
+import avisaai.modelo.entidade.comentario.resposta.Resposta;
+import avisaai.modelo.entidade.incidente.Incidente;
+import avisaai.modelo.entidade.usuario.Usuario;
+
+public interface ComentarioDAO {
+
+	void inserirComentario(Comentario comentario);
+
+	void deletarComentario(Comentario comentario);
+
+	void atualizarComentario(Comentario comentario);
+
+	List<Comentario> recuperarComentarios();
+
+	List<Comentario> consultarComentarioIncidente(Incidente incidente);
+
+	List<Comentario> consultarComentarioUsuario(Usuario usuario);
+
+	List<Resposta> consultarQuantidadeRespostasComentario(Comentario comentario);
+
+	Comentario consultarComentarioId(String id);
+}
