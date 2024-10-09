db188c68b4a6baf24f4c79af46fa8cf60dd065a0
Daiane Darosci <daiane.darosci@gmail.com>
2024-10-09 08:55:17 -0300

    Criado DAO da classe Usuário"

---
+++ src/main/java/avisaai/modelo/dao/usuario/UsuarioDAO.java
@@ -0,0 +1,20 @@
+package avisaai.modelo.dao.usuario;
+
+import java.util.List;
+
+import avisaai.modelo.entidade.usuario.Usuario;
+
+public interface UsuarioDAO {
+
+	void inserirUsuario(Usuario usuario);
+
+	void deletarUsuario(Usuario usuario);
+
+	void atualizarUsuario(Usuario usuario);
+
+	List<Usuario> consultarUsuarioNome(Usuario usuario);
+
+	List<Usuario> recuperarUsuarios();
+
+	Usuario consultarUsuarioId(Long id);
+}
